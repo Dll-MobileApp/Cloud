0280464c1e6ff0ddae7dc8a2200f9e93496dc5d6
SantiagoVelasquez119 <santiagovelas119@gmail.com>
2022-11-21 15:28:02 -0500

    Sprint 2 Terminado

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,12 +12,33 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalle sitio turístico POI'),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text("Nombre POI" )
-
+          children: [
+            Container(
+              child: const Text('Nombre POI', style:TextStyle(fontSize: 30, fontStyle: FontStyle.normal),),
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+              margin: EdgeInsets.all(30),
+            ),
+            Container(
+              child: const Image(image: AssetImage('assets/Logo.png'), width: 120, height: 120,),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Column(
+                children: const [
+                  Text('Ciudad: XXXXXXXX\nDepartamento: XXXXXXX\nTemperatura: XX'
+                      '\n\nDescripción: Lorem Ipsum is simply dummy text of the'
+                      ' printing and typesetting industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\n'
+                      'Otra información de interés', style: TextStyle(fontSize: 20),)
+                ],
+              ),
+            )
           ],
         ),
       ),
